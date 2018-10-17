31a74c471e1e71330ea60bc146b0b1fc2c631c95
Yassine AZZOUT <yassine@synthesio.com>
2018-10-17 13:20:08 +0200

    feat(events): save events into google calendar

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,16 @@
-const twosday = require('./index');
+const sessions = require('./sessions');
+const MENTOREE = require('./mentoree.json');
 const MENTORS = require('./people.json');
+const twosday = require('./index');
 
-const MENTOREE = {'name': 'Beckett', 'email': 'awesome@mentoree.com'};
-const events = twosday(MENTOREE, MENTORS);
+const events = sessions(MENTOREE, MENTORS);
 
 console.log(JSON.stringify(events, null, 2));
+
+async function bulk () {
+  const result = await twosday(MENTOREE, MENTORS);
+
+  console.log(result);
+}
+
+bulk();
